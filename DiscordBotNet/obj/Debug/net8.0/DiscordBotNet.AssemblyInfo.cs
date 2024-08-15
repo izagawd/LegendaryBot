f6d6410d15204bd1d2cde01354e91911060a8a9b
Emmanuel Onyekwelu <theizaboss@gmail.com>
2024-08-15 06:17:51 +0100

    push

--- DiscordBotNet/obj/Debug/net8.0/DiscordBotNet.AssemblyInfo.cs
+++ DiscordBotNet/obj/Debug/net8.0/DiscordBotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b02a0baa29b954f3658b582e31bdf0940429a1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c9da7516fa678085e45bfce559180ea82183df")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
