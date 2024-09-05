345714ff81351527b62349e5410ff6c87facb424
Emmanuel Onyekwelu <theizaboss@gmail.com>
2024-09-05 09:07:44 -0300

    push

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c827b989ce779a0cec7a826107cc406a248fcb41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6da0bbb99a54373e9d504141305cb3cabbbcbbe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
