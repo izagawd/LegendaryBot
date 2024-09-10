f3661dbce664b8186cdf350819467cfff32cdd94
Emmanuel Onyekwelu <theizaboss@gmail.com>
2024-09-10 18:30:22 -0300

    PUSH

---
+++ Website/Pages/Characters/CharacterInfo.cs
@@ -0,0 +1,47 @@
+using System.Text.Json.Serialization;
+
+namespace Website.Pages.Characters;
+
+public partial class CharacterInfo
+{
+    public class CharacterDto
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string ImageUrl { get; set; }
+        
+    }
+    public class GearStatDto
+    {
+        public string StatName { get; set; }
+        public bool IsPercentage { get; set; }
+        public int Value { get; set; }
+        public bool IsMainStat { get; set; }
+    }
+    public class GearDto
+    {
+        public long Id { get; set; }
+        public string GearName { get; set; }
+
+        public string ImageUrl { get; set; }
+        public string? OriginalOwnerImageUrl { get; set; }
+        public GearStatDto[] GearStats { get; set; }
+        
+        [JsonIgnore]
+        public GearStatDto MainStat => GearStats.First(i => i.IsMainStat);
+    }
+    public class CharacterInfoDto
+    {
+        public CharacterDto CharacterDto { get; set; }
+        public GearDto[] AllGears { get; set; }
+        [JsonIgnore]
+        public IEnumerable<GearDto> CharacterEquippedGears
+        {
+            get
+            {
+                return AllGears.Where(j => CharacterEquippedGearsId.Contains(j.Id));
+            }
+        }
+        public List<long> CharacterEquippedGearsId { get; set; }
+    }
+}
---
+++ WebsiteApi/Apis/CharacterInfoPageController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebsiteApi.Apis;
+
+[Route("[controller]")]
+[ApiController]
+public class CharacterInfoPageController : ControllerBase
+{
+    
+}
