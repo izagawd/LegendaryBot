8a3d30f868abe8e8a3257c27cb727c1c40b0059c
Emmanuel Onyekwelu <theizaboss@gmail.com>
2024-09-14 11:17:59 -0300

    push

--- WebsiteApi/Apis/GearsPageController.cs
+++ WebsiteApi/Apis/GearsPageController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Immutable;
+using System.Diagnostics;
 using BasicFunctionality;
 using DatabaseManagement;
 using Entities.LegendaryBot;
@@ -26,6 +27,7 @@ public class GearsPageController(PostgreSqlContext post) : ControllerBase
             .Where(i => i.UserData!.DiscordId == zaId)
          
             .ToArrayAsync();
+
         var dto = gottenCollection.Select(i => new Gears.GearDto
         {
             Name = i.Name,
@@ -40,6 +42,7 @@ public class GearsPageController(PostgreSqlContext post) : ControllerBase
                 IsPercentage = j.IsPercentage
             }).ToArray()
         }).ToArray();
+
         return Ok(new Gears.GearPageDto()
         {
             Gears = dto,
