1a9d8b16fe6e0f4a665c2dcc89ea216173190ed9
Emmanuel Onyekwelu <theizaboss@gmail.com>
2024-09-16 11:33:55 -0300

    push

--- PublicInfo/obj/Debug/net8.0/PublicInfo.AssemblyInfo.cs
+++ PublicInfo/obj/Debug/net8.0/PublicInfo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PublicInfo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+005d779dd72bd1698b8da39e1dd79806ed370e82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58df5060f47ab349029db205fc7b2476603891d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PublicInfo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PublicInfo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
