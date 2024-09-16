f478cee50e8788b48e1273d63a042943e314dc15
Emmanuel Onyekwelu <theizaboss@gmail.com>
2024-09-15 22:11:47 -0300

    push

--- DatabaseManagement/obj/Debug/net8.0/DatabaseManagement.AssemblyInfo.cs
+++ DatabaseManagement/obj/Debug/net8.0/DatabaseManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+487c9b448cf84dfb8ff5701436a56e39df135b2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9de34cd7cca1ae499ff2635006b7b51896f4514")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PublicInfo/obj/Debug/net8.0/PublicInfo.AssemblyInfo.cs
+++ PublicInfo/obj/Debug/net8.0/PublicInfo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PublicInfo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+487c9b448cf84dfb8ff5701436a56e39df135b2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9de34cd7cca1ae499ff2635006b7b51896f4514")]
 [assembly: System.Reflection.AssemblyProductAttribute("PublicInfo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PublicInfo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
