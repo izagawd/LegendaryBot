391f4fb7a99d092ffcde55d0fc8447e9a0ccb6dc
Emmanuel Onyekwelu <theizaboss@gmail.com>
2024-09-27 16:00:23 -0300

    push

--- DatabaseManagement/obj/Debug/net8.0/DatabaseManagement.AssemblyInfo.cs
+++ DatabaseManagement/obj/Debug/net8.0/DatabaseManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c97583c11d3e3a8d2c683bbfbc527bb263bab47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2a28c60a4ff6bf650b39f68d9e2f1ce956695f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
